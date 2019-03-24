7a6e21acf6df3a62960c59ef0d4e2714fbc00bb9
Mabdulah <Mabdulah596@hotmail.com>
2019-03-24 14:14:13 +0500

    updates

--- users/users.js
+++ users/users.js
@@ -2,10 +2,18 @@ const mongoose = require('mongoose');
 
 var usersSchema = mongoose.Schema({
     name: String,
-    price: Number,
-    quantity:Number,
-    warranty: String,
-    date: { type : Date, default: Date.now },
+    neck: Number,
+    shoulder:Number,
+    hlafChest: Number,
+    fullChest: Number,
+    sleeveLength: Number,
+    kameezLength: Number,
+    kameezWidth: Number,
+    wristWidth: Number,
+    shalwarInseam:Number,
+    shalwarLength:Number,
+    shalwarPancha:Number,
+    dateofjoining: { type : Date, default: Date.now },
 })
 
 var users = mongoose.model('users', usersSchema);
